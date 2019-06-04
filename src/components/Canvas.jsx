db6fb15654fadb9bf9acd344981c3fa642a52cf9
Kevin Baktiar <afterafx@gmail.com>
2019-06-03 17:22:44 -0700

    if game hasnt started, shows title. When game has started, title disappears

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,20 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
+import StartGame from './StartGame';
+import Title from './Title';
 
-const Canvas = () => {
-  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+const Canvas = props => {
+  const gameHeight = 1200;
+  const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
 
   return (
-    <svg id="aliens-go-home-canvas" preserveAspectRatio="xMaxYMax none" viewBox={viewBox}>
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
+      <CannonBall position={{ x: 0, y: -100 }} />
+      <CurrentScore score={15} />
+
+      {!props.gameState.started && (
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      )}
+
+      {props.gameState.started && (
+        <g>
+          <FlyingObject position={{ x: -150, y: -300 }} />
+          <FlyingObject position={{ x: 150, y: -300 }} />
+        </g>
+      )}
+
+      <Heart position={{ x: -300, y: 35 }} />
     </svg>
   );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
